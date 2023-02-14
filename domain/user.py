dece3fad5adb3cbfa8a88bd55663dcd68de12a7b
qiuqfang <2312180277@qq.com>
2023-02-14 23:28:40 +0800

    update

--- domain/user.py
+++ domain/user.py
@@ -10,6 +10,13 @@ class CreateUser(BaseModel):
     @validator("name")
     def validator_name(cls, value, values, config, field):
         print(cls, value, values, config, field)
-        if '2' == value:
-            raise ValueError("参数有误")
+        if len(value) < 2 or len(value) > 10:
+            raise ValueError("姓名长度应不小于2且不大于10")
+        return value
+
+    @validator("password")
+    def validator_password(cls, value, values, config, field):
+        print(cls, value, values, config, field)
+        if len(value) < 6 or len(value) > 20:
+            raise ValueError("密码长度应不小于6且不大于10")
         return value
